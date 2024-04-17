afa35a577cc3b556f88ebe8bc2a4378bdfd515d6
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-04-17 09:12:37 +0530

    Initial commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            //Admin
+            [
+                'name' => 'Gayathra Dilshan',
+                'username' => 'gayathra',
+                'email' => 'gayathradilshan1@gmail.com',
+                'password' => Hash::make('Dilshan2002@!@'),
+                'role' => 'admin',
+                'status' => 'active',
+            ],
+
+
+
+        ]);
+    }
+}
