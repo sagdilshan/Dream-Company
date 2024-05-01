37acfb9d7301e72f8fd69b99531f2ac20a13a075
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-05-01 22:01:30 +0530

    Update header.blade.php

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -15,7 +15,7 @@
 
       {{-- <li class="nav-item">
         <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
+          <i class="fas d fa-expand-arrows-alt"></i>
         </a>
       </li> --}}
       <li class="nav-item">
