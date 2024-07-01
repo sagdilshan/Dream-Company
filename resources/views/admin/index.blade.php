dbded1bccd5ca07006d1e47e55f085046abcfe1e
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-07-01 22:41:01 +0530

    Update index.blade.php

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,32 @@
+@extends('home.dash-header')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Admin Dashboard')
+@section('content')
+
+
+ <!-- Page Header Start -->
+ {{-- <div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container">
+        <h1 class="display-3 mb-4 animated slideInDown">Admin Dashboard</h1>
+        <nav aria-label="breadcrumb animated slideInDown">
+            <ol class="breadcrumb mb-0">
+                <li class="breadcrumb-item"><a class="text-danger" href="{{ url('/') }}">Home</a></li>
+
+                <li class="breadcrumb-item active" aria-current="page">Admin Dashboard</li>
+            </ol>
+        </nav>
+    </div>
+</div> --}}
+<!-- Page Head End -->
+<br><br><br><br>
+    <!-- Contact Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+
+
+            
+        </div>
+    </div>
+    <!-- Contact End -->
+
+
+@endsection
