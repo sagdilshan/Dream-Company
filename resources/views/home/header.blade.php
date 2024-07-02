7b7a94b9662ad6b7b33f3b46c2099aff6a7dbd03
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-07-02 15:28:47 +0530

    Update header.blade.php

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -95,17 +95,54 @@
                     <a href="{{ route('contact') }}"
                         class="nav-item nav-link {{ Route::is('contact') ? 'active' : '' }}">Contact</a>
 
-                    <a href="{{ route('login') }}"
-                        class="nav-item d-lg-none d-block nav-link {{ Route::is('login') ? 'active' : '' }}">Login</a>
+                    @if (Route::has('login'))
+                        @auth
+                            @if (Auth::user()->role == 'admin')
+                                <li><a class="nav-item nav-link" href="{{ url('/admin-index') }}">Dashboard</a></li>
+                            @elseif(Auth::user()->role == 'guest')
+                                <li><a class="nav-item nav-link" href="{{ url('/dashboard') }}">Dashboard</a></li>
+                            @endif
+
+                        @endauth
+                    @endif
+                    @if (Route::has('login'))
+                        @auth
+                            <a href="{{ route('admin.logout') }}" class="nav-item d-lg-none d-block nav-link">Logout</a>
+                        @else
+                            <a href="{{ route('login') }}"
+                                class="nav-item d-lg-none d-block nav-link {{ Route::is('login') ? 'active' : '' }}">Login</a>
+                        @endauth
+                    @endif
 
                 </div>
-                <div class="d-none d-lg-flex ms-2">
-                    <a class="btn btn-light btn-sm-square rounded-circle ms-3" href="{{ route('login') }}"
-                        title="Login">
-                        <small class="bi bi-box-arrow-right" style="color:rgb(255, 0, 0);"></small>
-                    </a>
 
-                </div>
+
+                @if (Route::has('login'))
+                    @auth
+                        @if (Auth::user()->role == 'admin')
+                            <div class="d-none d-lg-flex ms-2">
+                                <a class="btn btn-light btn-sm-square rounded-circle ms-3"
+                                    href="{{ route('admin.logout') }}" title="Log Out">
+                                    <small class="fa fa-power-off" style="color:rgb(255, 0, 0);"></small>
+                                </a>
+                            </div>
+                        @elseif(Auth::user()->role == 'guest')
+                            <div class="d-none d-lg-flex ms-2">
+                                <a class="btn btn-light btn-sm-square rounded-circle ms-3"
+                                    href="{{ route('admin.logout') }}" title="Log Out">
+                                    <small class="fa fa-power-off" style="color:rgb(255, 0, 0);"></small>
+                                </a>
+                            </div>
+                        @endif
+                    @else
+                        <div class="d-none d-lg-flex ms-2">
+                            <a class="btn btn-light btn-sm-square rounded-circle ms-3" href="{{ route('login') }}"
+                                title="Login">
+                                <small class="bi bi-box-arrow-right" style="color:rgb(255, 0, 0);"></small>
+                            </a>
+                        </div>
+                    @endauth
+                @endif
             </div>
         </nav>
     </div>
