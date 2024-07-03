519504847afc05745c04446a95461999023f5ced
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-07-03 19:32:34 +0530

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\CustomerModel;
 use App\Models\FeedbackModel;
+use App\Models\InquireModel;
+use App\Models\ProjectModel;
 use App\Models\StaffModel;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -17,8 +19,10 @@ class AdminController extends Controller
     public function AdminDashboard()
     {
 
-
-        return view('admin.index');
+        $pending_project = ProjectModel::where('project_status', 'pending')
+            ->orderBy('created_at', 'desc')
+            ->get();
+        return view('admin.index', compact('pending_project'));
     }
 
     public function AdminLogout(Request $request)
@@ -68,31 +72,57 @@ class AdminController extends Controller
 
     public function AdminProject()
     {
-        return view('admin.project.all-project');
+        $completed_project = ProjectModel::whereIn('project_status', ['completed', 'cancel'])
+            ->orderBy('created_at', 'desc')
+
+            ->get();
+
+        $pending_project = ProjectModel::where('project_status', 'pending')
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        $customers = CustomerModel::all();
+        return view('admin.project.all-project', compact('customers', 'completed_project', 'pending_project', 'completed_project'));
     }
 
     public function AdminFeedback()
     {
-        return view('admin.feedback.all-feedback');
+        $feedbacks = FeedbackModel::orderBy('created_at', 'desc')
+        // ->where('status', 'show')
+        ->get();
+
+        return view('admin.feedback.all-feedback', compact('feedbacks'));
     }
 
     public function AdminStaff()
     {
-        return view('admin.staff.all-staff');
+        $allstaff = StaffModel::orderBy('created_at', 'desc')
+        // ->where('status', 'show')
+        ->get();
+        return view('admin.staff.all-staff', compact('allstaff'));
     }
 
     public function AdminClient()
     {
-        return view('admin.client.all-client');
+        $allclient = CustomerModel::orderBy('created_at', 'desc')
+        ->get();
+        return view('admin.client.all-client', compact('allclient'));
     }
 
     public function AdminInquire()
     {
-        return view('admin.inquire.all-inquire');
+        $allinquir = InquireModel::orderBy('created_at', 'desc')
+        ->where('status', 'new')
+        ->get();
+        return view('admin.inquire.all-inquire', compact('allinquir'));
     }
     public function AdminAll()
     {
-        return view('admin.all-admin.all-admin');
+        $alladmins = User::where('role', 'admin')
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return view('admin.all-admin.all-admin', compact('alladmins'));
     }
 
     public function AdminProfile()
@@ -212,6 +242,101 @@ class AdminController extends Controller
 
         return redirect()->route('admin.staff')->with($notification);
 
+    }
+
+    public function AdminProjectStore(Request $request)
+    {
+        $projec = new ProjectModel();
+        $projec->p_name = trim($request->p_name);
+        $projec->s_name = trim($request->s_name);
+        $projec->p_link = trim($request->p_link);
+        $projec->association = trim($request->association);
+        $projec->assign_customer = trim($request->assign_customer);
+        $projec->p_fee = trim($request->p_fee);
+        $projec->s_month = trim($request->s_month);
+        $projec->description = trim($request->description);
+        $projec->created_by = Auth::user()->id;
+        $projec->project_status = trim($request->project_status);
+
+
+        if (empty($request->f_month)) {
+            $projec->f_month = 'Present';
+        } else {
+            $projec->f_month = $request->f_month;
+        }
+
+
+
+        if ($request->file('thumb')) {
+            $file = $request->file('thumb');
+            $filename = date('Ymd') . $file->getClientOriginalName(); // 0215.a.gayathr.png
+            $file->move(public_path('upload/project_images'), $filename);
+            $projec['thumb'] = $filename;
+        }
+
+        if ($request->hasFile('photo')) {
+            foreach ($request->file('photo') as $image) {
+                $filename = date('Ymd') . '_' . $image->getClientOriginalName(); // Generate unique filename
+                $image->move(public_path('upload/project_images'), $filename); // Move image to storage directory
+                $imagePaths[] = 'upload/project_images/' . $filename; // Store image path in an array
+            }
+            $projec['photo'] = json_encode($imagePaths); // Save array of image paths as JSON string in database
+        }
+
+
+        $projec->save();
+
+        $notification = array(
+            'message' => 'New Project Listed',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('admin.project')->with($notification);
+
+    }
+
+    public function AdminStore(Request $request)
+    {
+        $admins = new User;
+        $admins->name = trim($request->name);
+        $admins->email = trim($request->email);
+        $admins->username = trim($request->username);
+        $admins->created_by = Auth::user()->id;
+        $admins->nic = trim($request->nic);
+        $admins->address = trim($request->address);
+        $admins->phone = trim($request->phone);
+        $admins->password = Hash::make($request->password);
+        $admins->role = 'admin';
+
+
+        $admins->save();
+
+        $notification = array(
+            'message' => 'New Admin Listed',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('admin.all')->with($notification);
+
+    }
+
+    public function AdminInquireStatus($id)
+    {
+        $inqu = InquireModel::find($id);
+
+        if ($inqu) {
+            $inqu->update(['status' => 'replied']);
+            $notification = array(
+                'message' => 'Inquire Status Updated',
+                'alert-type' => 'success'
+            );
+            return redirect()->back()->with($notification);
+        }
+        $notification = array(
+            'message' => 'Inquire Status Updated',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
     }
 
 }
