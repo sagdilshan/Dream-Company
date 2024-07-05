ea5f9cc7400c8cb3aad4818929a008bed45f8bd1
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-07-05 17:24:50 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -257,19 +257,19 @@
             <div class="row g-5">
                 <div class="col-sm-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.1s">
                     <i class="fa fa-users fa-3x text-white mb-3"></i>
-                    <h1 class="display-4 text-white" data-toggle="counter-up">100</h1>
+                    <h1 class="display-4 text-white" data-toggle="counter-up">{{$formattedTotalcustomers}}</h1>
                     <span class="fs-5 text-white">Happy Clients</span>
                     <hr class="bg-white w-25 mx-auto mb-0">
                 </div>
                 <div class="col-sm-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.3s">
                     <i class="fa fa-check fa-3x text-white mb-3"></i>
-                    <h1 class="display-4 text-white" data-toggle="counter-up">100</h1>
+                    <h1 class="display-4 text-white" data-toggle="counter-up">{{$formattedPending_project}}</h1>
                     <span class="fs-5 text-white">Projects Completed</span>
                     <hr class="bg-white w-25 mx-auto mb-0">
                 </div>
                 <div class="col-sm-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.5s">
                     <i class="fa fa-users-cog fa-3x text-white mb-3"></i>
-                    <h1 class="display-4 text-white" data-toggle="counter-up">5</h1>
+                    <h1 class="display-4 text-white" data-toggle="counter-up">{{$all_staff}}</h1>
                     <span class="fs-5 text-white">Dedicated Staff</span>
                     <hr class="bg-white w-25 mx-auto mb-0">
                 </div>
