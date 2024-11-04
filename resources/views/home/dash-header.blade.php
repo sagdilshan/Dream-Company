928d709f65d7a54f0197b0f4561864a5a8a60528
Gayathra Dilshan <gayathradilshan1@gmail.com>
2024-11-04 16:23:07 +0530

    Update dash-header.blade.php

--- resources/views/home/dash-header.blade.php
+++ resources/views/home/dash-header.blade.php
@@ -86,7 +86,10 @@
 
         <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
             <a href="{{ url('/') }}" class="navbar-brand ms-4 ms-lg-0">
-                <h1 class="display-5 m-0" style="color:rgb(255, 0, 0);">Eversys Holdings</h1>
+                <h1 class="display-5 m-0"
+                style="background: radial-gradient(circle, #264242, #424242, #d32f2f, #b71c1c);-webkit-background-clip: text;-webkit-text-fill-color: transparent; background-clip: text; text-fill-color: transparent; text-align: center;">
+                EVERSYS HOLDINGS
+            </h1>
             </a>
             <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse"
                 data-bs-target="#navbarCollapse">
