896d8c8b4ca35906e71cd7618c721968d61dda06
Gayathra Dilshan <gayathradilshan1@gmail.com>
2025-01-09 00:05:33 +0530

    Update dash-header.blade.php

--- resources/views/home/dash-header.blade.php
+++ resources/views/home/dash-header.blade.php
@@ -85,7 +85,7 @@
         </div>
 
         <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
-            <a href="{{ url('/') }}" class="navbar-brand ms-4 ms-lg-0">
+            <a href="{{ route('software') }}" class="navbar-brand ms-4 ms-lg-0">
                 <h1 class="display-5 m-0"
                 style="background: radial-gradient(circle, #264242, #424242, #d32f2f, #b71c1c);-webkit-background-clip: text;-webkit-text-fill-color: transparent; background-clip: text; text-fill-color: transparent; text-align: center;">
                 Eversys Technology
