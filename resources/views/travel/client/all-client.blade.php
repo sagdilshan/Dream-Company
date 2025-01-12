9d47c0b9e32e762fbfa77371fe7415ad2f65cc34
Gayathra Dilshan <gayathradilshan1@gmail.com>
2025-01-12 20:00:05 +0530

    Create all-client.blade.php

---
+++ resources/views/travel/client/all-client.blade.php
@@ -0,0 +1,175 @@
+@extends('home.travel-dash-header')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Travel Customers')
+@section('content')
+
+
+
+    <!-- Page Head End -->
+    <br><br><br><br>
+    <!-- Contct Start -->
+    <div class="container-xxl py-5">
+
+
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <h2 style="font-family: Open Sans;">Travel Customers</h2>
+
+                            <br>
+                            <table id="example3" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Customer Name</th>
+                                        <th>Email</th>
+                                        <th>Phone</th>
+                                        <th>Address</th>
+
+                                        <th>Action</th>
+                                    </tr>
+
+                                </thead>
+                                <tbody>
+
+
+                                    @foreach ($allclient as $key => $item)
+                                        <tr>
+                                            <td>{{ $key + 1 }}</td>
+
+                                            <td>{{ $item->cus_name }}</td>
+                                            <td>{{ $item->email }}</td>
+                                            <td>{{ $item->phone }}</td>
+                                            <td>{{ $item->address }}</td>
+
+                                            <td>
+                                                <a href="{{ route('travel.edit.client', $item->id) }}" class="btn btn-outline-success">Edit</a>
+                                            </td>
+
+
+
+                                        </tr>
+                                    @endforeach
+
+
+                                    </tfoot>
+                            </table>
+                        </div>
+                        <!-- /.crd-bod -->
+                    </div>
+
+
+                </div>
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+
+
+
+        <br>
+
+
+        <section class="content ">
+            <div class="container-fluid ">
+                <div class="row">
+
+                    <!-- /.col -->
+                    <div class="col-lg-12">
+                        <div class="card">
+
+                            <div class="card-body">
+                                <div class="tab-content">
+
+                                    <h2 class="mb-4" style="font-family: Open Sans;color: red;">Add Customers</h2>
+
+
+                                    <div class="active tab-pane" id="settings">
+                                        <form method="post" action="{{ route('travel.client.store') }}">
+                                            @csrf
+
+                                            <div class="form-group row mt-3">
+                                                <label class="col-sm-2 col-form-label" style="font-weight: 600;">Customer
+                                                    Name</label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" class="form-control" name= "cus_name"
+                                                        placeholder="Enter customer name" required>
+
+
+                                                </div>
+                                            </div>
+
+
+                                            <div class="form-group row mt-3">
+                                                <label class="col-sm-2 col-form-label"
+                                                    style="font-weight: 600;">Email</label>
+                                                <div class="col-sm-10">
+                                                    <input type="email" class="form-control" name="email"
+                                                        placeholder="Enter email" required>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group row mt-3">
+                                                <label class="col-sm-2 col-form-label"
+                                                    style="font-weight: 600;">Address</label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" class="form-control" name="address"
+                                                        placeholder="Enter address" required>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group row mt-3">
+                                                <label class="col-sm-2 col-form-label"
+                                                    style="font-weight: 600;">Phone</label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" class="form-control" name="phone"
+                                                        placeholder="Enter contact number" required>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row mt-3">
+                                                <label class="col-sm-2 col-form-label" style="font-weight: 600;">NIC</label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" class="form-control" name="nic"
+                                                        placeholder="Enter NIC number (optional)" >
+                                                </div>
+                                            </div>
+                                            <div class="form-group row mt-3">
+                                                <label class="col-sm-2 col-form-label" style="font-weight: 600;">Passport Number</label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" class="form-control" name="passport"
+                                                        placeholder="Enter Passport number (optional)" >
+                                                </div>
+                                            </div>
+
+
+
+
+                                            <div class="form-group row mt-3">
+                                                <div class="offset-sm-2 col-sm-10 d-flex  justify-content-end">
+                                                    <button type="submit" class="btn btn-danger">Save Changes</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+
+
+                                    <!-- /.tab-pane -->
+                                </div>
+                                <!-- /.tab-content -->
+                            </div><!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
+    </div>
+    <!-- Contact End -->
+
+
+@endsection
