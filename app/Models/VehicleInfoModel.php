c1b02ff36bfdf9da7e979c70e8e7c11a4b9d65aa
Gayathra Dilshan <gayathradilshan1@gmail.com>
2025-01-13 18:38:28 +0530

    Create VehicleInfoModel.php

---
+++ app/Models/VehicleInfoModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VehicleInfoModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'vehicle_info';
+    protected $guarded = [];
+}
