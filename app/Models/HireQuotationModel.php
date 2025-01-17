44d70a69817997efba30ecfe02a925b1d8581191
Gayathra Dilshan <gayathradilshan1@gmail.com>
2025-01-17 19:46:54 +0530

    Create HireQuotationModel.php

---
+++ app/Models/HireQuotationModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HireQuotationModel extends Model
+{
+    use HasFactory;
+    protected $table = 'hire_quotation';
+    protected $guarded = [];
+}
