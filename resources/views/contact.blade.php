787f75f0d10eaa5bd75aeb66981b0a58f9999543
Gayathra Dilshan <gayathradilshan1@gmail.com>
2025-07-14 22:24:45 +0400

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,7 @@
 <head>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
     </head>
-    <!-- Page Header Start -->
+    <!-- Page Header Start jbjb-->
     <div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container">
             <h1 class="display-3 mb-4 animated slideInDown">Contact</h1>
